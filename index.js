f808a03efbae9e2fab367f5446ad0a488df51f09
Julius <porfin@MacBook-Pro-de-Julio-Olivares.local>
2024-10-08 00:46:49 -0300

    agregando repertorio.json

--- index.js
+++ index.js
@@ -9,3 +9,12 @@ app.listen(3000, console.log("Servidor Levantado en el puerto 3000!"));
 app.use(express.json());
 
 const index = path.join(__dirname, "index.html");
+
+app.get("/", (req, res) => {
+  res.sendFile(index);
+});
+
+app.get("/canciones", (req, res) => {
+  const canciones = JSON.parse(fs.readFileSync("repertorio.json"));
+  res.json(canciones);
+});
