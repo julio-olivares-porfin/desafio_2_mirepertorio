5ee527440247ac3274482af3ba900f730df02a7d
Julius <porfin@MacBook-Pro-de-Julio-Olivares.local>
2024-10-08 01:03:49 -0300

    agregando try catch

--- index.js
+++ index.js
@@ -15,33 +15,64 @@ app.get("/", (req, res) => {
 });
 
 app.get("/canciones", (req, res) => {
-  const canciones = JSON.parse(fs.readFileSync("repertorio.json"));
-  res.json(canciones);
+  try {
+    const canciones = JSON.parse(fs.readFileSync("repertorio.json"));
+    res.json(canciones);
+  } catch (error) {
+    res.status(500).send("Error al leer el repertorio.");
+  }
 });
 
 app.post("/canciones", (req, res) => {
   const rep = req.body;
-  const repertorio = JSON.parse(fs.readFileSync("repertorio.json"));
-  repertorio.push(rep);
-  fs.writeFileSync("repertorio.json", JSON.stringify(repertorio));
-  res.send("Canción agregada con Exito");
+  try {
+    const repertorio = JSON.parse(fs.readFileSync("repertorio.json"));
+    repertorio.push(rep);
+    fs.writeFileSync("repertorio.json", JSON.stringify(repertorio));
+    res.send("Canción agregada con éxito!");
+  } catch (error) {
+    res.status(500).send("Error al agregar la canción.");
+  }
 });
 
 app.put("/canciones/:id", (req, res) => {
   const { id } = req.params;
   const rep = req.body;
-  const repertorio = JSON.parse(fs.readFileSync("repertorio.json"));
-  const index = repertorio.findIndex((p) => p.id == id);
-  repertorio[index] = rep;
-  fs.writeFileSync("repertorio.json", JSON.stringify(repertorio));
-  res.send("Canción modificada con éxito");
+  try {
+    const repertorio = JSON.parse(fs.readFileSync("repertorio.json"));
+    const index = repertorio.findIndex((p) => p.id == id);
+
+    if (index === -1) {
+      return res.status(404).send("Canción no encontrada.");
+    }
+
+    repertorio[index] = rep;
+    fs.writeFileSync("repertorio.json", JSON.stringify(repertorio));
+    res.send("Canción modificada con éxito");
+  } catch (error) {
+    res.status(500).send("Error al modificar la canción.");
+  }
 });
 
 app.delete("/canciones/:id", (req, res) => {
   const { id } = req.params;
-  const repertorio = JSON.parse(fs.readFileSync("repertorio.json"));
-  const index = repertorio.findIndex((p) => p.id == id);
-  repertorio.splice(index, 1);
-  fs.writeFileSync("repertorio.json", JSON.stringify(repertorio));
-  res.send("Canción eliminada con éxito");
+  try {
+    const repertorio = JSON.parse(fs.readFileSync("repertorio.json"));
+    const index = repertorio.findIndex((p) => p.id == id);
+
+    if (index === -1) {
+      return res.status(404).send("Canción no encontrada.");
+    }
+
+    repertorio.splice(index, 1);
+    fs.writeFileSync("repertorio.json", JSON.stringify(repertorio));
+    res.send("Canción eliminada con éxito");
+  } catch (error) {
+    res.status(500).send("Error al eliminar la canción.");
+  }
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Algo salió mal.");
 });
